709eed81cd40eee3a087afb597107da4e176a1f1
Vasily Markov <mvv1993Vasya@gmail.com>
2024-02-02 14:26:45 +0300

    row constructor from container

--- include/matrices.hpp
+++ include/matrices.hpp
@@ -5,35 +5,81 @@
 #include <cassert>
 namespace matrices {
 
-template<typename T>
-struct row_t {
-    size_t size;
-    T* data = nullptr;
-    row_t() {
-    };
-    row_t(size_t n):size(n), data(new T[size]) 
-    {
-    }
-    ~row_t() { delete [] data;}
+template<typename T> void construct(T* ptr, T& rhs) { new (ptr) T(rhs);}
+template<typename T> void construct(T* ptr, T&& rhs) { new (ptr) T(std::move(rhs));}
 
-    void allocate(size_t n) {
-        size = n;
-        data = new T[size];
+template<typename T> void destroy(T* ptr) {ptr->~T();}
+template<typename It> void destroy(It begin, It end) {
+    while(begin != end) {
+        destroy(&*begin++);
+    }
+}
+
+template<typename T> struct row_buf_t {
+protected:
+    size_t size_, used_ = 0;
+    T* data_ = nullptr;
+protected:
+    row_buf_t(size_t n = 0):data_(n == 0 ? nullptr : static_cast<T*>(::operator new(sizeof(T)*n))), size_(n) {}
+    row_buf_t(const row_buf_t& buf) = delete;
+    row_buf_t& operator=(const row_buf_t&) = delete; 
+    row_buf_t(row_buf_t&& buf) noexcept: size_(buf.size_()), used_(buf.used_), data_(buf.data_) {
+        buf.data_ = nullptr;
+        buf.size_ = 0;
+        buf.used_ = 0;
+    };
+
+    row_buf_t& operator=(row_buf_t&& buf) noexcept {
+        std::swap(data_, buf.data_()); 
+        std::swap(size_, buf.size_());
+        std::swap(used_, buf.used_());
+        return *this;
+    };
+    ~row_buf_t() {
+        destroy(data_, data_ + used_);
+        ::operator delete(data_);
+    };
+};
+
+template<typename T> struct row_t: private row_buf_t<T> {
+    using row_buf_t<T>::used_;
+    using row_buf_t<T>::size_;
+    using row_buf_t<T>::data_;
+    row_t() {};
+    explicit row_t(size_t n = 0): row_buf_t<T>(n) {}
+    template<typename It> row_t(It begin, It end): row_buf_t<T>(std::distance(begin, end)) {
+        while(begin != end) {
+            construct(data_ + used_, *begin);
+            used_++;
+            begin++;
+        }
+    }
+
+    row_t(const row_t& rhs): row_buf_t<T>(rhs.used_){
+        while(used_ < rhs.used_) {
+            construct(data_, rhs.data_[used_]);
+            used_++;
+        }
+    }
+    row_t& operator=(const row_t& rhs) {
+        row_t tmp(rhs);
+        std::swap(*this, tmp);
+        return *this;
     }
     T& operator[](size_t n)  
     {
-        if(n > size)
+        if(n > used_)
             throw std::out_of_range("Out of range");
-        return data[n];
+        return data_[n];
     }
     const T& operator[](size_t n) const
     {
-        if(n > size)
+        if(n > used_)
             throw std::out_of_range("Out of range");
-        return data[n];
+        return data_[n];
     }
     friend std::ostream& operator<<(std::ostream& os, const row_t& r) {
-        for(size_t i = 0; i < r.size; ++i) {
+        for(size_t i = 0; i < r.used_; ++i) {
             os << r[i] << ' ';
         }
     return os;
@@ -44,8 +90,11 @@ template<typename T>
 struct matr_t {
     size_t size;
     row_t<T>* rows = nullptr;
-    matr_t(size_t n):size(n), rows(new row_t<T>[size]) {
-        allocate();
+    matr_t(size_t n){
+
+    }
+    template<typename It> matr_t(size_t n, It begin, It end): matr_t(n) {
+
     }
     // static matr_t eye(size_t n) {
     //     size = n;
@@ -53,7 +102,6 @@ struct matr_t {
     //     return *this;
     // }
     ~matr_t() {
-        delete [] rows;
     }
     row_t<T>& operator[](size_t n)
     {
--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,21 @@
 #include <optional>
 #include <exception>
 #include <matrices.hpp>
+#include <iterator>
 #define TEST
 
 using namespace matrices;
 
 template <typename T>
 void print(const std::vector<T>& vec) {
-    for(auto const& i : vec) {
-        std::cout << i << ' ';
-    }
+    std::copy(std::begin(vec), std::end(vec), std::ostream_iterator<T>(std::cout, ", "));
     std::cout << std::endl;
 }
 
 int main() {
-
+    std::vector<int> data = {1,2,3,4,5};
+    row_t<int> r(std::begin(data), std::end(data));
+    std::cout << r << std::endl;
 #ifndef TEST
     size_t N{0};
     std::cin >> N;
@@ -27,17 +28,16 @@ int main() {
         data[i] = tmp;
     }  
 #else
-    std::vector<double> input = {1,2,3,4};
-    matr_t<int> m(3);
-    try
-    {
-        std::cout << m << std::endl;
-    } 
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    
+    // std::vector<int> input = {1,2,3,4};
+    // row_t<int> r(std::begin(input), std::end(input));
+    // try
+    // {
+    //     std::cout << r << std::endl;
+    // } 
+    // catch(const std::exception& e)
+    // {
+    //     std::cerr << e.what() << '\n';
+    // }
 #endif
 
     return 0;
