72a03e8eda8283c1fd58215b3d4993ca103b049a
Vasily Markov <mvv1993Vasya@gmail.com>
2024-02-17 16:00:07 +0300

    Generators

---
+++ tests/generators.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+def wellMatrix(size):
+    pivot = 1.1
+    det = pivot**size
+    diag = np.zeros((size,size))
+    np.fill_diagonal(diag, pivot)
+    LU = np.random.randint(1, 2, (size,size))
+    I = np.identity(size)
+    L = np.tril(LU, -1) + I
+    U = np.triu(LU, 1) + I
+    matrix = L @ diag @ U
+    cond = np.linalg.cond(matrix)
+
+    return "Well Matrix", matrix, det, cond
+
+
+def illMatrix(size):
+    pivots = np.array(size)
+
+    small_pivots = np.random.normal(0, 0.01, size//2)
+    if size % 2 != 0:
+        big_pivots = np.random.randint(100, 10000, (size//2)+1)
+    else:
+        big_pivots = np.random.randint(100, 10000, (size//2))  
+    
+    pivots = np.random.permutation(np.concatenate((small_pivots, big_pivots), None))
+    det = np.prod(pivots)
+    diag = np.zeros((size,size))
+    np.fill_diagonal(diag, pivots)
+    LU = np.random.randint(1, 5, (size,size))
+    I = np.identity(size)
+    L = np.tril(LU, -1) + I
+    U = np.triu(LU, 1) + I
+
+    matrix = matrix = L @ diag @ U
+    cond = np.linalg.cond(matrix)
+    return "Ill Matrix", matrix, det, cond
+
+
+def degenerateMatrix(size):
+    row = np.random.normal(0, 2, (size))
+    coeffs = np.random.normal(0, 2, (size))
+    matrix =  row * coeffs[:, None]
+    return "Degen Matrix", matrix, 0, None
+
+
+def nonSquareMatrix(size):
+    row = np.random.randint(1, size+1)
+    col = np.random.randint(1, size+1)
+    if col == row:
+        if col == 1:
+            col += 1
+        else:
+            col -= 1
+    matrix = np.random.normal(0, 2, (row, col))
+    return matrix
+
+
+print(nonSquareMatrix(5))
--- tests/tests.py
+++ tests/tests.py
@@ -2,44 +2,7 @@ import numpy as np
 import time
 from subprocess import Popen, PIPE, STDOUT
 import logging
-
-def generateMatrix(size):
-    pivot = 1.1
-    det = pivot**size
-    diag = np.zeros((size,size))
-    np.fill_diagonal(diag, pivot)
-    LU = np.random.randint(1, 2, (size,size))
-    I = np.identity(size)
-    L = np.tril(LU, -1) + I
-    U = np.triu(LU, 1) + I
-    matrix = L @ diag @ U
-    cond = np.linalg.cond(matrix)
-
-    return "WellcondMatrix", matrix, det, cond
-
-
-def generateIllConditionMatrix(size):
-    pivots = np.array(size)
-
-    small_pivots = np.random.normal(0, 0.01, size//2)
-    if size % 2 != 0:
-        big_pivots = np.random.randint(100, 10000, (size//2)+1)
-    else:
-        big_pivots = np.random.randint(100, 10000, (size//2))  
-    
-    pivots = np.random.permutation(np.concatenate((small_pivots, big_pivots), None))
-    det = np.prod(pivots)
-    diag = np.zeros((size,size))
-    np.fill_diagonal(diag, pivots)
-    LU = np.random.randint(1, 5, (size,size))
-    I = np.identity(size)
-    L = np.tril(LU, -1) + I
-    U = np.triu(LU, 1) + I
-
-    matrix = matrix = L @ diag @ U
-    cond = np.linalg.cond(matrix)
-    return "Illcond Matrix", matrix, det, cond
-
+import generators as gen
 
 def serialize(matrix):
     out_matrix = np.ndarray.flatten(matrix)
@@ -57,9 +20,12 @@ def serialize(matrix):
 def runTest(app, generator, size):
     error = False
     gen_name, matrix, det, cond = generator(size)
-    app_det = float(runApp(app, serialize(matrix)))
-    if abs(app_det-det) > 0.1:
-        error = True
+    try:
+        app_det = float(runApp(app, serialize(matrix)))
+        if abs(app_det-det) > 0.00001:
+            error = True
+    except Exception as e:
+        raise  Exception(e, gen_name)
     return gen_name, error, det, app_det, cond
 
 
@@ -70,22 +36,21 @@ def parseAppOut(out):
             raise Exception(out)
     return out
 
+
 def runApp(app, data):
     pipe = Popen([app], stdout=PIPE, stdin=PIPE)
     out = pipe.communicate(input=bytes(data, "UTF-8"))
     return parseAppOut(out[0].decode())
 
 
-COND_TRESHOLD = 1e10
-
-
 def end_to_end(app, n):
+    COND_TRESHOLD = 1e10
     print("Running tests...")
     logging.basicConfig(filename="tests/results.log", format='%(asctime)s: %(levelname)s %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
     try:
 
         for i in range(2,n+1):
-            gen_name, err, det, app_det, cond = runTest(app, generateMatrix, i)
+            gen_name, err, det, app_det, cond = runTest(app, gen.wellMatrix, i)
             if err and cond >  COND_TRESHOLD:
                 logging.error(f'Runing {gen_name} test. Matrix size: {i}. Ill conditional number. Error: {abs(det-app_det)}')
             elif err and not cond:
@@ -93,7 +58,7 @@ def end_to_end(app, n):
             else:
                 logging.info(f'Runing {gen_name} test. Matrix size: {i}. Test passed')
 
-            gen_name, err, det, app_det, cond = runTest(app, generateIllConditionMatrix, i)
+            gen_name, err, det, app_det, cond = runTest(app, gen.illMatrix, i)
             if err and cond >  COND_TRESHOLD:
                 logging.error(f'Runing {gen_name} test. Matrix size: {i}. Ill conditional number. Error: {abs(det-app_det)}')
             elif err and not cond:
@@ -103,9 +68,16 @@ def end_to_end(app, n):
         print("Tests finished.")
         
     except Exception as e:
-        logging.critical(f'Runing {gen_name} test. Matrix size: {i}. Error: {e}')
+        # logging.critical(f'Runing {gen_name} test. Matrix size: {i}. Error: {e}')
         print("Tests failed.")
     
+
+import string
+import random
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
 
 if __name__ == '__main__':
     app = "./matrices"
