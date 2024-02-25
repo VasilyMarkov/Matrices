247acb39ea78fd55237355111cd5b76281654fad
Vasily Markov <mvv1993Vasya@gmail.com>
2024-02-26 00:38:29 +0300

    New tests

--- tests/matrix_tests.cpp
+++ tests/matrix_tests.cpp
@@ -6,7 +6,28 @@
 using namespace testing;
 using namespace matrices;
 
+TEST(Row, constructor) {
+    std::vector<int> vec = {1,2,3,4};
+    row_t<int> row(std::begin(vec), std::end(vec));
+    EXPECT_EQ(row[0], 1);
+    EXPECT_EQ(row[1], 2);
+    EXPECT_EQ(row[2], 3);
+    EXPECT_EQ(row[3], 4);
+}
+
 TEST(Row, equalOperator) {
+    std::vector<double> ref = {2,4,1,5,2,1,2,3,4};
+    row_t<double> r_ref{std::begin(ref), std::end(ref)};
+
+    row_t<double> r_vec{std::begin(ref), std::end(ref)};
+    EXPECT_TRUE(r_vec == r_ref);
+
+    std::vector<double> ref_eps = {2+eps,4,1,5,2-eps,1,2,3,4};
+    row_t<double> r_ref_eps{std::begin(ref_eps), std::end(ref_eps)};
+    EXPECT_FALSE(r_ref_eps == r_ref);
+}
+
+TEST(Row, equalOperatorInt) {
     std::vector<double> ref = {2,4,1,5,2,1,2,3,4};
     row_t<double> r_ref{std::begin(ref), std::end(ref)};
 
@@ -29,14 +50,33 @@ TEST(Row, subtractionAssignmentOperator) {
     EXPECT_TRUE(r == r_ref);
 }
 
-TEST(Row, divisionAssignmentOperator) {
-    std::vector<double> vec = {2,4,6,8};
-    row_t<double> m{std::begin(vec), std::end(vec)};
-    m /= 2;
-    std::vector<double> ref = {1,2,3,4};
-    row_t<double> m_ref{std::begin(ref), std::end(ref)};
-    EXPECT_TRUE(m == m_ref);
+TEST(Row, swap) {
+    std::vector<int> vec1 = {6,7,8,9};
+    row_t<int> row1{std::begin(vec1), std::end(vec1)};
 
+    std::vector<int> vec2 = {1,2,3,4};
+    row_t<int> row2{std::begin(vec2), std::end(vec2)};
+
+    swap(row1, row2);
+
+    EXPECT_EQ(row1[0], 1);
+    EXPECT_EQ(row1[1], 2);
+    EXPECT_EQ(row1[2], 3);
+    EXPECT_EQ(row1[3], 4);
+
+    EXPECT_EQ(row2[0], 6);
+    EXPECT_EQ(row2[1], 7);
+    EXPECT_EQ(row2[2], 8);
+    EXPECT_EQ(row2[3], 9);
+}
+
+TEST(Matrix, constructor) {
+    std::vector<int> vec = {2,4,1,5};
+    matr_t<int> matr(2, std::begin(vec), std::end(vec));
+    EXPECT_EQ(matr[0][0], 2);
+    EXPECT_EQ(matr[0][1], 4);
+    EXPECT_EQ(matr[1][0], 1);
+    EXPECT_EQ(matr[1][1], 5);
 }
 
 TEST(Matrix, equalOperator) {
